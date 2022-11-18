ef746d981b72cf4eb5f3691052b6987e4091c5b9
Alex <Alex@lyj.com>
2022-11-18 09:11:53 +0800

    2022.11.18

---
+++ src/November_2022/Solution_2022_11_18.java
@@ -0,0 +1,20 @@
+package November_2022;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+public class Solution_2022_11_18 {
+    public int sumSubseqWidths(int[] nums) {
+        final int MOD = 1000000007;
+        Arrays.sort(nums);
+        long res = 0;
+        long x = nums[0], y = 2;
+        for (int j = 1; j < nums.length; j++) {
+            res = (res + nums[j] * (y - 1) - x) % MOD;
+            x = (x * 2 + nums[j]) % MOD;
+            y = y * 2 % MOD;
+        }
+        return (int) ((res + MOD) % MOD);
+    }
+}
